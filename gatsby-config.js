f5d91f465104402599533b1e2212ba703ff525cd
JUEsoft <emmanueljunior433@gmail.com>
2020-07-11 02:51:26 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emmamuel Emejulu Portfolio`,
-        short_name: `Emmanuel Emejulu Portfolio`,
+        name: `Emmamuel Emejulu's Portfolio`,
+        short_name: `Emmanuel Emejulu's Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
